40b9beacfc5946742c30a72723a5d13b3ab1535a
Stas Globus <stani246@gmail.com>
2019-12-07 22:01:29 +0200

    grade average is working

--- EX2/Testgrades.c
+++ EX2/Testgrades.c
@@ -36,7 +36,69 @@ static int file(char *dir)
 
 	return grade;
 }
+int find_minimum(int array[], int array_size) {
+	int c, index = 0, min=101;
 
+	for (c = 0; c < array_size; c++)
+		if (array[c] < min && array[c] != -1) {
+			index = c;
+			min = array[c];
+		}
+
+	return index;
+}
+float Average(int sum, int size) {
+	return (sum / (float)size);
+}
+
+
+float CalculateGrade(int grades_array[])
+{
+
+	int local_grade_array[FILE_TYPES] = { 0 }, ex_arr[EXERESICES]={0},
+		min_grade_index, sum = 0, 
+		counter = 0, exam_grade, midterm;
+	float final_avg = 0, ex_avg = 0;
+	// copy the data to local array for calculations
+	for (int i = 0; i < FILE_TYPES; i++) {
+		local_grade_array[i]= grades_array[i];
+		if (i < EXERESICES)
+			ex_arr[i] = grades_array[i];
+	}
+	//delete two lowers exersice scores
+	for (int i = 0; i < LOW_SCORES; i++) {
+		min_grade_index = find_minimum(ex_arr, EXERESICES);
+		ex_arr[min_grade_index] = -1;
+	}
+	// zero scores that are lower then PASS Grade
+	for (int i = 0; i < FILE_TYPES; i++) {
+		if (local_grade_array[i] != -1 && local_grade_array[i] < LOWEST_GRADE) {
+			local_grade_array[i] = 0;
+		}
+	}
+	//make an average of exersices grades
+	for (int i = 0; i < EXERESICES; i++) {
+		if (ex_arr[i] != -1) {
+			sum = sum + ex_arr[i];
+			counter++;
+		}
+	}
+	if (VALID_EX == counter) { // check we have 8 valid exersice grades
+		ex_avg = Average(sum, counter);
+	}
+	
+	//take the midterm even if it's 0
+	midterm = local_grade_array[MIDTERM];
+	// check if student went to moed B
+	if (0 == local_grade_array[MOEDB])
+		exam_grade = local_grade_array[MOEDA];
+	else
+		exam_grade = local_grade_array[MOEDB];
+
+	// final calculation
+	final_avg = 0.2*exam_grade + 0.2*midterm + 0.6*exam_grade;
+	return final_avg;
+}
 DWORD WINAPI GetGradesThread(LPVOID lpParam)//, char *dir)
 {
 	Sleep(10);
@@ -44,7 +106,9 @@ DWORD WINAPI GetGradesThread(LPVOID lpParam)//, char *dir)
 	DWORD p_thread_ids[FILE_TYPES];
 	DWORD exit_code;
 	Grades *grades = (Grades *)lpParam;
-	int ret_val;
+	int local_grades[13] = { 0 };
+	int ret_val, grades_array[FILE_TYPES] = { 0 };
+	float final_grade;
 	char file_names[FILE_TYPES][MAX_STRING] = { "ex01.txt","ex02.txt","ex03.txt","ex04.txt","ex05.txt",
 					"ex06.txt","ex07.txt","ex08.txt","ex09.txt","ex10.txt",
 					"midterm.txt","moedA.txt","moedB.txt" };
@@ -63,23 +127,26 @@ DWORD WINAPI GetGradesThread(LPVOID lpParam)//, char *dir)
 	//open each thread to look for specific file
 	for (int i = 0; i < FILE_TYPES; i++) {
 
-		//p_thread_handles[i] = CreateThreadSimple(PullGrade, &p_thread_ids[i], grade->dir);
+		
 		strcpy(temp_dir[i], grades->dir);
 		strcat(temp_dir[i], file_names[i]);
 		p_thread_handles[i] = CreateThreadSimple(PullGrade, temp_dir[i], &p_thread_ids[i]);
 		
-		//();
-		//Sleep(1);//Integration
-		//memset(temp_dir[i], '\0', MAX_STRING);
+		
 	}
-	DWORD dwRet = WaitForMultipleObjects(FILE_TYPES, p_thread_handles, FALSE, INFINITE);
-	for (int i = 1; i < FILE_TYPES+1; i++) {
-		ret_val = GetExitCodeThread(p_thread_handles[i-1], &exit_code);
-		printf("file %d exit code is %d\n", (i-1), exit_code);
+	DWORD dwRet = WaitForMultipleObjects(FILE_TYPES, p_thread_handles, TRUE, INFINITE);
+	for (int i = 0; i < FILE_TYPES; i++) {
+		ret_val = GetExitCodeThread(p_thread_handles[i], &exit_code);
+		local_grades[i] = exit_code;
+		printf("file %d exit code is %d\n", i, exit_code);
 	}
-	//ret_val = GetExitCodeThread(p_thread_handles[5], &exit_code);
-	//printf("the return value is%d\n", ret_val);
+
+
+	grades->grade_value = local_grades;
+	final_grade = CalculateGrade(local_grades);
+	printf("the final grade is%.2f", final_grade);
 	getchar();
+
 	
 	return 0;
 }
@@ -87,16 +154,13 @@ DWORD WINAPI GetGradesThread(LPVOID lpParam)//, char *dir)
 
 DWORD WINAPI PullGrade(LPVOID lpParam)
 {
-	//Sleep(10);
+	Sleep(10);
 	char *file_dir = (char*)lpParam;
 	
 	int grade = 0;
-	
-	//memcpy(file_dir, ptr, DIR_SIZE); //copy the part before the left bracket
-	//strcat(file_dir, file_names[0]); // add the result to the new string
 	grade = file(file_dir);
 	//printf("%d\n", grade);
-	//getchar();
+
 	
 	return grade;
 
--- EX2/Testgrades.h
+++ EX2/Testgrades.h
@@ -7,6 +7,15 @@
 #define MAX_STRING 50
 //#define FILE_TYPES 13
 #define FILENAME 20
+#define LOW_SCORES 2
+#define LOWEST_GRADE 2// chenge it to 60 later
+#define EXERESICES 10
+#define MIDTERM 10
+#define MOEDA 11
+#define MOEDB 12
+#define FINAL_GRADE 13
+#define VALID_EX 8
+
 
 
 // Includes --------------------------------------------------------------------
